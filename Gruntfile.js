767865a9b4bec968d7bc9417ffe5223349908075
callumsong <ghksong@gmail.com>
2015-02-04 11:19:41 -0800

    gruntfile, package.json. attempting bonus marks too

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,30 +3,39 @@
 module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
     jshint: {
       dev: {
         options: {
-          node: true,
-          globals: {
-            describe: true,
-            it: true,
-            before: true,
-            after: true
-          }
+          jshintrc: '.jshintrc',
         },
-        src: ['Gruntfile.js', 'greet.js', 'test/**/*.js']
+      src: ['Gruntfile.js', 'greet.js', 'test/**/*.js']
       }
     },
-
     simplemocha: {
       all: {
         src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      files: ['**/*', '.gitignore', '!package.json', '.jshintrc'],
+      tasks: ['jshint', 'simplemocha']
+    },
+    jscs: {
+      all: {
+        options: {
+          "standard": "google"
+        },
+        files: {
+          src: ['Gruntfile.js', 'greet.js', 'test/**/*.js']
+        }
       }
     }
   });
   grunt.registerTask('test', ['simplemocha']);
   grunt.registerTask('syntax', ['jshint']);
+  grunt.registerTask('spy', ['watch']);
   grunt.registerTask('default', ['test', 'syntax']);
 };
